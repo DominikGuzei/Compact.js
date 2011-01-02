a26edc4ea952f838ee09800cad555c9717d015b7
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-02 18:23:16 +0100

    added namespace context to .end()

--- scripts/Class.js
+++ scripts/Class.js
@@ -1,18 +1,18 @@
 define(function() {
 	
 	var Class = function(_classpath) {
-		var classpath = _classpath.split(".");
-		var namespace = window;
-		var classname = classpath[classpath.length - 1];
 		
-		// build the namespace for the class
-		var next = window[classpath[0]] || (window[classpath[0]] = {});
-		for (var i=1; i < classpath.length; i++) {
-			next[classpath[i]] || (next[classpath[i]] = {});
-			namespace = next;
-			next = next[classpath[i]];
-		}
+		var classpath; // the full class path as array of strings
+		var namespace; // reference to the last namespace object before class
+		var classname; // name of the class as string
 		
+		/**
+		 * Creates a getter function for a property on the prototype to
+		 * minimize the glue code you have to write for your class
+		 * 
+		 * @param {string} propName name of the property
+		 * @param {object} prop the property definition object
+		 */
 		var defineGetter = function(propName, prop) {
 			camelPropName = propName.replace(/\S/, propName[0].toUpperCase());
 			namespace[classname].prototype["get" + camelPropName] = prop.getter ? prop.getter :
@@ -21,6 +21,15 @@ define(function() {
 				};
 		}
 		
+		/**
+		 * Creates a setter function for a property on the prototype.
+		 * It implements hooks for filtering, validating and before/after
+		 * property change handling. This allows to inject behaviour
+		 * involved with setting a property on the class.
+		 * 
+		 * @param {string} propName Name of the property
+		 * @param {object} prop Property definition object
+		 */
 		var defineSetter = function(propName, prop) {
 			camelPropName = propName.replace(/\S/, propName[0].toUpperCase());
 			
@@ -41,6 +50,13 @@ define(function() {
 			};
 		}
 		
+		/**
+		 * Takes a properties definition and creates the property as
+		 * pseudo private var on the prototype (_var) and defines getters
+		 * and setters for the property.
+		 * 
+		 * @param {object} properties The definition object passed in .properties() 
+		 */
 		var buildProperties = function(properties) {
 			for(var prop in properties) if(properties.hasOwnProperty(prop)) {
 					
@@ -52,12 +68,29 @@ define(function() {
 			}
 		};
 		
+		/**
+		 * Takes the argument object passed on instanciation and
+		 * overwrites the default values with user arguments
+		 * 
+		 * @param {object} userArgs User arguments passed as object literal
+		 * @param {object} properties The property definition object
+		 */
 		var applyDefaultValues = function(userArgs, properties) {
-			for(var prop in properties) if(properties.hasOwnProperty(prop)) {
-				this["_"+prop] = userArgs[prop] || (properties[prop].value != undefined ? properties[prop].value : undefined);
+			for(var prop in userArgs) if(userArgs.hasOwnProperty(prop)) {
+				if(properties[prop]) {
+					this["_"+prop] = userArgs[prop];
+				}
 			}
 		};
 		
+		/**
+		 * Assigns all methods declared in the .methods() call
+		 * to the prototype of the class. On sub classes it saves
+		 * a reference to the overwritten super method in each method. 
+		 * 
+		 * @param {function} superclass The base class to extend from
+		 * @param {object} methods The method definition object
+		 */
 		var buildMethods = function(superclass, methods) {
 			
 			for(var method in methods) if(methods.hasOwnProperty(method)) {
@@ -80,8 +113,10 @@ define(function() {
 			
 		}
 		
-		// return an anonymous object that
-		// manages how to construct the class
+		/**
+		 * Return a chainable builder object that saves the
+		 * given input and composes the class at the .end() 
+		 */
 		return {
 			superclass: null,
 			classConstructor: function() {},
@@ -113,9 +148,28 @@ define(function() {
 				return this;
 			},
 			
-			end: function() {
+			/**
+			 * Construct the class with all given information passed in
+			 * by the other calls.
+			 * 
+			 * @param {object} context The object the namespace is applied to
+			 */
+			end: function(context) {
 				var builder = this;
 				
+				// Construct the namespace for the class
+				classpath = _classpath.split(".");
+				namespace = context;
+				classname = classpath[classpath.length - 1];
+
+				var next = namespace[classpath[0]] || (namespace[classpath[0]] = {});
+				for (var i=1; i < classpath.length; i++) {
+					next[classpath[i]] || (next[classpath[i]] = {});
+					namespace = next;
+					next = next[classpath[i]];
+				}
+				
+				// Define a class constructor if it is not a mixin
 				if(this.classConstructor) {
 					var klass = namespace[classname] = function() {
 						var userArgs = arguments[0] || {};
@@ -123,14 +177,15 @@ define(function() {
 						applyDefaultValues.call(this, userArgs, builder.classProperties);
 						builder.classConstructor.call(this);
 					}
-				
 					klass.constructor = klass;
 				}
 				
+				// copy the superclass prototype to the class prototype
 				if(builder.superclass) {
 					namespace[classname].prototype.__proto__ = builder.superclass.prototype;
 				}
 				
+				// copy all mixins prototype methods to the class prototype
 				if(builder.mixinClasses) {
 					for(var i=0; i < builder.mixinClasses.length; i++) {
 						var mixin = builder.mixinClasses[i].prototype;

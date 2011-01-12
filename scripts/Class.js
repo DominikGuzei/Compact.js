fc18d409c077ab7c7ad3b1bfbeb8061e61950f7a
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-12 11:58:57 +0100

    Added deep copy of complex property objects and watchable properties

--- scripts/Class.js
+++ scripts/Class.js
@@ -1,122 +1,12 @@
-define(function() {
+define(['Util'], function(Util) {
 	
 	var Class = function(_classpath) {
-		
-		var classpath; // the full class path as array of strings
-		var namespace; // reference to the last namespace object before class
-		var classname; // name of the class as string
-		
-		/**
-		 * Creates a getter function for a property on the prototype to
-		 * minimize the glue code you have to write for your class
-		 * 
-		 * @param {string} propName name of the property
-		 * @param {object} prop the property definition object
-		 */
-		var defineGetter = function(propName, prop) {
-			camelPropName = propName.replace(/\S/, propName[0].toUpperCase());
-			namespace[classname].prototype["get" + camelPropName] = prop.getter ? prop.getter :
-				function() { 
-					return this["_" + propName]; 
-				};
-		}
-		
-		/**
-		 * Creates a setter function for a property on the prototype.
-		 * It implements hooks for filtering, validating and before/after
-		 * property change handling. This allows to inject behaviour
-		 * involved with setting a property on the class.
-		 * 
-		 * @param {string} propName Name of the property
-		 * @param {object} prop Property definition object
-		 */
-		var defineSetter = function(propName, prop) {
-			camelPropName = propName.replace(/\S/, propName[0].toUpperCase());
-			
-			namespace[classname].prototype["set"+camelPropName] = function(value) {
-				var validated = true;
-				
-				this._filterChange && typeof(this._filterChange) == 'function' && (value = this._filterChange(propName, value));
-				this._beforeChange && typeof(this._beforeChange) == 'function' && (validated = this._beforeChange(propName, value));
-				
-				if(validated) {	
-					if(prop.setter) {
-						prop.setter.call(this, value);
-					}	else {
-						this["_" + propName] = value;
-					}
-					this._afterChange && typeof(this._afterChange) == 'function' && this._afterChange(propName, value);
-				}
-			};
-		}
-		
-		/**
-		 * Takes a properties definition and creates the property as
-		 * pseudo private var on the prototype (_var) and defines getters
-		 * and setters for the property.
-		 * 
-		 * @param {object} properties The definition object passed in .properties() 
-		 */
-		var buildProperties = function(properties) {
-			for(var prop in properties) if(properties.hasOwnProperty(prop)) {
-					
-				if(namespace[classname].prototype["_" +prop] == undefined) {
-					namespace[classname].prototype["_"+prop] = (properties[prop].value != undefined ? properties[prop].value : undefined);
-					defineGetter(prop, properties[prop]);
-					defineSetter(prop, properties[prop]);
-				}
-			}
-		};
-		
-		/**
-		 * Takes the argument object passed on instanciation and
-		 * overwrites the default values with user arguments
-		 * 
-		 * @param {object} userArgs User arguments passed as object literal
-		 * @param {object} properties The property definition object
-		 */
-		var applyDefaultValues = function(userArgs, properties) {
-			for(var prop in userArgs) if(userArgs.hasOwnProperty(prop)) {
-				if(properties[prop]) {
-					this["_"+prop] = userArgs[prop];
-				}
-			}
-		};
-		
-		/**
-		 * Assigns all methods declared in the .methods() call
-		 * to the prototype of the class. On sub classes it saves
-		 * a reference to the overwritten super method in each method. 
-		 * 
-		 * @param {function} superclass The base class to extend from
-		 * @param {object} methods The method definition object
-		 */
-		var buildMethods = function(superclass, methods) {
-			
-			for(var method in methods) if(methods.hasOwnProperty(method)) {
-				namespace[classname].prototype[method] = (superclass && superclass.prototype[method]) ?
-					(function(name, fn) {
-						return function() {
-	            // add a reference to the same method on the super class
-	            this.superMethod = superclass.prototype[name];
-	            // apply the current context with arguments and superMethod reference
-	            var ret = fn.apply(this, arguments);
-							// we dont need the reference anymore, so delete it.
-							delete this.superMethod;
-							// return the result of the applied method
-	            return ret;
-						}
-					})(method, methods[method])
-					:
-					methods[method];
-			}
-			
-		}
 		
 		/**
 		 * Return a chainable builder object that saves the
 		 * given input and composes the class at the .end() 
 		 */
+		
 		return {
 			superclass: null,
 			classConstructor: function() {},
@@ -158,49 +48,40 @@ define(function() {
 				var builder = this;
 				
 				// Construct the namespace for the class
-				classpath = _classpath.split(".");
-				namespace = context;
-				classname = classpath[classpath.length - 1];
+				var classpath = _classpath.split("."); // the full class path as array of strings
+				var namespace = context; // reference to the last namespace object before class
+				var classname = classpath[classpath.length - 1]; // name of the class as string
 
-				var next = namespace[classpath[0]] || (namespace[classpath[0]] = {});
-				for (var i=1; i < classpath.length; i++) {
-					next[classpath[i]] || (next[classpath[i]] = {});
-					namespace = next;
-					next = next[classpath[i]];
-				}
+				namespace = Util.buildNamespace(namespace, classpath);
 				
 				// Define a class constructor if it is not a mixin
-				if(this.classConstructor) {
-					var klass = namespace[classname] = function() {
-						var userArgs = arguments[0] || {};
-						builder.superclass && builder.superclass.apply(this, arguments);
-						applyDefaultValues.call(this, userArgs, builder.classProperties);
-						builder.classConstructor.call(this);
-					}
-					klass.constructor = klass;
-				}
+				var klass = namespace[classname] = function() {
+					var userArgs = arguments[0] || {};
+					builder.superclass && builder.superclass.apply(this, arguments);
+					Util.addInstanceProperties(this, userArgs, builder.classProperties);
+					builder.classConstructor.call(this);
+				};
+				
+				klass.constructor = klass;
 				
 				// copy the superclass prototype to the class prototype
 				if(builder.superclass) {
 					namespace[classname].prototype.__proto__ = builder.superclass.prototype;
 				}
 				
-				// copy all mixins prototype methods to the class prototype
 				if(builder.mixinClasses) {
 					for(var i=0; i < builder.mixinClasses.length; i++) {
-						var mixin = builder.mixinClasses[i].prototype;
-						for(var prop in mixin) if(mixin.hasOwnProperty(prop)) {
-							namespace[classname].prototype[prop] = mixin[prop];
-						}
+						Util.addProperties(this.classProperties, this.mixinClasses[i].__properties__, false);
+						Util.addProperties(this.classMethods, this.mixinClasses[i].__methods__, false);
 					}
 				}
 				
-				buildProperties(builder.classProperties);
-				buildMethods.call(klass, this.superclass, this.classMethods);
+				Util.buildPropertyAccessors(klass.prototype, builder.classProperties);
+				Util.addMethods(klass.prototype, this.classMethods, this.superclass);
 			}
 			
 		}; // end return
-	}
+	};
 	
 	return Class;
 });
--- tests/spec/scripts/ClassSpec.js
+++ tests/spec/scripts/ClassSpec.js
@@ -1,5 +1,11 @@
 
-define(['Class'], function(Class) {
+define(['Class', 'Mixin'], function(Class, Mixin) {
+	
+	var complexProp = {
+		object: {},
+		array: [],
+		date: new Date()
+	}
 	
 	Class("com.example.Person")
 	.properties({
@@ -9,221 +15,228 @@ define(['Class'], function(Class) {
 		custom: {
 			value: "some",
 			getter: function() { return this._custom + "custom"; },
-			setter: function(value) { this._custom = value + "changed" }
+			setter: function(value) { this._custom = value + "changed" },
+			watchable: true
 		},
-		strange: {}
+		watched: {
+			value: "watch",
+			watchable: true
+		},
+		strange: {},
+		complex: {
+			value: complexProp
+		}
 	})
 	.end(window);
 	
 	
 	describe("Class", function() {	
-		it("should provide a namespace", function() {
-			Class("com.example.Test").end(window);
-			expect(com.example.Test).toBeDefined();
+		
+		describe("Class()", function() {
+		  it("should provide a namespace", function() {
+				Class("com.example.Test").end(window);
+				expect(com.example.Test).toBeTypeOf('function');
+			});
 		});
 		
-	});
-	
-	
-	describe(".extend()", function() {
-	  Class("test.extend.Super")
-		.end(window);
 
-		Class("test.extend.Sub").extend(test.extend.Super)
-		.end(window);
-		
-		it("should be instance of both classes", function() {
-		  var sub = new test.extend.Sub();
-			expect(sub instanceof test.extend.Super && sub instanceof test.extend.Sub).toBeTruthy();
-		});
-	});
-	
-	
-	describe(".methods()", function() {
-	  Class("test.methods.Super")
-		.methods({
-			sayHello: function() { return "hello" }
-		})
-		.end(window);
+		describe(".extend()", function() {
+		  Class("test.extend.Super")
+			.end(window);
 
-		Class("test.methods.Sub").extend(test.methods.Super)
-		.methods({
-			sayHello: function() { 
-				return this.superMethod() + " world"; 
-			}
-		})
-		.end(window);
-		
-		it("should add methods to the prototype of the class", function() {
-			expect(test.methods.Super.prototype.sayHello).toBeTypeOf('function');
-			expect(test.methods.Sub.prototype.sayHello).toBeTypeOf('function');
+			Class("test.extend.Sub").extend(test.extend.Super)
+			.end(window);
+			
+			it("should be instance of both classes", function() {
+			  var sub = new test.extend.Sub();
+				
+				expect(sub instanceof test.extend.Super && sub instanceof test.extend.Sub).toBeTruthy();
+			});
 		});
-		
-		it("should reference the super class method in the sub class", function() {
-		  var sub = new test.methods.Sub();
-			expect(sub.sayHello()).toEqual("hello world");
-		});
-	});
-	
-	
-	describe(".initialize()", function() {
-		var init = jasmine.createSpy();
-	  Class("test.initialize.Super")
-		.initialize(init)
-		.end(window);
-		
-		var initSub = jasmine.createSpy();
-		Class("test.initialize.Sub").extend(test.initialize.Super)
-		.initialize(initSub)
-		.end(window);
-		
-		it("should call the initialize function on instanciation", function() {
-			expect(init).not.toHaveBeenCalled();
-			var initTest = new test.initialize.Super();
-			expect(init).toHaveBeenCalled();
-		});
-		
-		it("should call the initialize function on the super class automatically", function() {
-		  expect(initSub).not.toHaveBeenCalled();
-			var initTest = new test.initialize.Sub();
-			expect(initSub).toHaveBeenCalled();
-		});
-		
-		it("should have default and user arguments applied on initialize", function() {
-		  
-			Class("test.initialize.Properties")
-			.properties({
-				defaultVar: {
-					value: "somevalue"
-				},
-				userChanged: {
-					value: "defaultvalue"
+
+
+		describe(".methods()", function() {
+		  Class("test.methods.Super")
+			.methods({
+				sayHello: function() { return "hello" }
+			})
+			.end(window);
+
+			Class("test.methods.Sub").extend(test.methods.Super)
+			.methods({
+				sayHello: function() { 
+					return this.superMethod() + " world"; 
 				}
 			})
-			.initialize(function() {
-				expect(this.getDefaultVar()).toEqual("somevalue");
-				expect(this.getUserChanged()).toEqual("uservalue");
+			.end(window);
+
+			it("should add methods to the prototype of the class", function() {
+				expect(test.methods.Super.prototype.sayHello).toBeTypeOf('function');
+				expect(test.methods.Sub.prototype.sayHello).toBeTypeOf('function');
+			});
+
+			it("should reference the super class method in the sub class", function() {
+			  var sub = new test.methods.Sub();
+				expect(sub.sayHello()).toEqual("hello world");
+			});
+		});
+
+
+		describe(".initialize()", function() {
+			var init = jasmine.createSpy();
+		  Class("test.initialize.Super")
+			.initialize(init)
+			.end(window);
+
+			var initSub = jasmine.createSpy();
+			Class("test.initialize.Sub").extend(test.initialize.Super)
+			.initialize(initSub)
+			.end(window);
+
+			it("should call the initialize function on instanciation", function() {
+				expect(init).not.toHaveBeenCalled();
+				var initTest = new test.initialize.Super();
+				expect(init).toHaveBeenCalled();
+			});
+
+			it("should call the initialize function on the super class automatically", function() {
+			  expect(initSub).not.toHaveBeenCalled();
+				var initTest = new test.initialize.Sub();
+				expect(initSub).toHaveBeenCalled();
+			});
+
+			it("should have default and user arguments applied on initialize", function() {
+
+				Class("test.initialize.Properties")
+				.properties({
+					defaultVar: {
+						value: "somevalue"
+					},
+					userChanged: {
+						value: "defaultvalue"
+					}
+				})
+				.initialize(function() {
+					expect(this.defaultVar).toEqual("somevalue");
+					expect(this.userChanged).toEqual("uservalue");
+				})
+				.end(window);
+				var property = new test.initialize.Properties({
+					userChanged: "uservalue"
+				});
+			});
+
+		});
+
+
+
+		describe(".properties()", function() {
+
+			var person;
+			beforeEach(function() {
+				person = new com.example.Person({
+					name: "Dominik"
+				});
+			});
+
+			it("should apply user arguments / leave the default / set to undefined", function() {
+			  expect(person.name).toEqual("Dominik");
+				expect(person._custom).toEqual("some");
+				expect(person._strange).toEqual(undefined);
+			});
+
+			it("should add public getter and setter for properties", function() {
+			  expect(com.example.Person.prototype["getCustom"]).toBeTypeOf('function');
+				expect(com.example.Person.prototype["setCustom"]).toBeTypeOf('function');
+				expect(person.getCustom()).toEqual("somecustom");
+				person.setCustom("Setter");
+				expect(person.getCustom()).toEqual("Setterchangedcustom");
+			});
+
+			it("should use custom getter and setter if defined", function() {
+			  expect(com.example.Person.prototype["getCustom"]).toBeTypeOf('function');
+				expect(com.example.Person.prototype["setCustom"]).toBeTypeOf('function');
+				expect(person.getCustom()).toEqual("somecustom");
+				person.setCustom("some");
+				expect(person.getCustom()).toEqual("somechangedcustom");
+			});
+			
+			it("should make a deep copy of complex properties (objects, arrays)", function() {
+			  expect(person.complex).not.toBe(complexProp);
+				expect(person.complex.object).not.toBe(complexProp.object);
+				expect(person.complex.array).not.toBe(complexProp.array);
+				expect(person.complex.date).not.toBe(complexProp.date);
+			});
+
+			describe("watchable: true", function() {
+				it("should call a 'filter' method before property change and apply the result", function() {
+					person._filterChange = function(property, value) {
+						expect(property).toEqual("watched");
+						expect(value).toEqual("Some Value");
+						return "Filtered Value";
+					}
+					spyOn(person, '_filterChange').andCallThrough();
+					person.setWatched("Some Value");
+					expect(person._filterChange).toHaveBeenCalled();
+					expect(person.watched).toEqual("Filtered Value");
+					person._filterChange = undefined;
+				});
+				
+				it("should call a 'before' method to validate the change of a property", function() {
+					person._beforeChange = function(property, value) {
+						expect(property).toEqual("watched");
+						return value == "Right Value" ? true : false;
+					}
+					spyOn(person, '_beforeChange').andCallThrough();
+					person.setWatched("Some Value");
+					expect(person._beforeChange).toHaveBeenCalled();
+					expect(person.watched).toEqual("watch");
+					person.setWatched("Right Value");
+					expect(person.watched).toEqual("Right Value");
+					person._beforeChange = undefined;
+				});
+
+				it("should call an 'after' method to inform about the change", function() {
+					person._afterChange = function(property, value) {
+						expect(property).toEqual("watched");
+						expect(value).toEqual("Changed Value");
+					}
+					spyOn(person, '_afterChange').andCallThrough();
+					person.setWatched("Changed Value");
+					expect(person._afterChange).toHaveBeenCalled();
+					expect(person.watched).toEqual("Changed Value");
+					person._afterChange = undefined;
+				});
+			});
+		});
+
+			
+
+		describe(".mixin()", function() {
+			Mixin("test.mixin.Mixin")
+			.methods({
+				sayHello: function() { return "hello" },
+				saySomethingElse: function() { return "mixed in method" }
+			})
+			.end(window);
+
+		  Mixin("test.mixin.Test")
+			.methods({
+				sayHello: function() { return "hello world" },
+				sayGoodbye: function() { return "goodbye" }
 			})
 			.end(window);
 			
-			var property = new test.initialize.Properties({
-				userChanged: "uservalue"
+			Class("test.mixin.Mixed")
+			.mixin(test.mixin.Mixin, test.mixin.Test)
+			.end(window);
+			
+			it("should add mixin methods to prototype of class", function() {
+			  expect(test.mixin.Mixed.prototype.saySomethingElse).toBeTypeOf('function');
+				expect(test.mixin.Mixed.prototype.sayHello).toBeTypeOf('function');
 			});
-		});
-	
-	});
-	
-	
-	
-	describe(".properties()", function() {
-		
-		var person;
-		beforeEach(function() {
-			person = new com.example.Person({
-				name: "Dominik"
-			});
+
 		});
 		
-		it("should add private properties to prototype", function() {
-			expect(com.example.Person.prototype._name).toBeDefined();
-			expect(com.example.Person.prototype._name).toEqual("Unknown");
-		});
-		
-		it("should apply user arguments / leave the default / set to undefined", function() {
-		  expect(person._name).toEqual("Dominik");
-			expect(person._custom).toEqual("some");
-			expect(person._strange).toEqual(undefined);
-		});
-		
-		it("should add public getter and setter for properties", function() {
-		  expect(com.example.Person.prototype["getName"]).toBeTypeOf('function');
-			expect(com.example.Person.prototype["setName"]).toBeTypeOf('function');
-			expect(person.getName()).toEqual("Dominik");
-			person.setName("Setter");
-			expect(person.getName()).toEqual("Setter");
-		});
-		
-		it("should use custom getter and setter if defined", function() {
-		  expect(com.example.Person.prototype["getCustom"]).toBeTypeOf('function');
-			expect(com.example.Person.prototype["setCustom"]).toBeTypeOf('function');
-			expect(person.getCustom()).toEqual("somecustom");
-			person.setCustom("some");
-			expect(person.getCustom()).toEqual("somechangedcustom");
-		});
-		
-		it("should also add getter and setter for undefined values", function() {
-		  expect(com.example.Person.prototype["getStrange"]).toBeTypeOf('function');
-			expect(com.example.Person.prototype["setStrange"]).toBeTypeOf('function');
-			expect(person.getStrange()).toEqual(undefined);
-			person.setStrange("defined");
-			expect(person.getStrange()).toEqual("defined");
-		});
-		
-		it("should call a 'filter' method before property change and apply the result", function() {
-			person._filterChange = function(property, value) {
-				expect(property).toEqual("name");
-				expect(value).toEqual("Some Value");
-				return "Filtered Value";
-			}
-			spyOn(person, '_filterChange').andCallThrough();
-			person.setName("Some Value");
-			expect(person._filterChange).toHaveBeenCalled();
-			expect(person.getName()).toEqual("Filtered Value");
-			person._filterChange = undefined;
-		});
-		
-		it("should call a 'before' method to validate the change of a property", function() {
-			person._beforeChange = function(property, value) {
-				expect(property).toEqual("name");
-				return value == "Right Value" ? true : false;
-			}
-			spyOn(person, '_beforeChange').andCallThrough();
-			person.setName("Some Value");
-			expect(person._beforeChange).toHaveBeenCalled();
-			expect(person.getName()).toEqual("Dominik");
-			person.setName("Right Value");
-			expect(person.getName()).toEqual("Right Value");
-			person._beforeChange = undefined;
-		});
-		
-		it("should call an 'after' method to inform about the change", function() {
-			person._afterChange = function(property, value) {
-				expect(property).toEqual("name");
-				expect(value).toEqual("Changed Value");
-			}
-			spyOn(person, '_afterChange').andCallThrough();
-			person.setName("Changed Value");
-			expect(person._afterChange).toHaveBeenCalled();
-			expect(person.getName()).toEqual("Changed Value");
-			person._afterChange = undefined;
-		});
-	});
-	
-	describe(".mixin()", function() {
-		Class("test.mixin.Mixin")
-		.methods({
-			sayHello: function() { return "hello" },
-			saySomethingElse: function() { return "mixed in method" }
-		})
-		.end(window);
-		
-	  Class("test.mixin.Test")
-		.methods({
-			sayHello: function() { return "hello world" },
-			sayGoodbye: function() { return "goodbye" }
-		})
-		.mixin(test.mixin.Mixin)
-		.end(window);
-		
-		it("should add mixin methods to prototype of class", function() {
-		  expect(test.mixin.Test.prototype.saySomethingElse).toBeTypeOf('function');
-			expect(test.mixin.Test.prototype.sayHello).toBeTypeOf('function');
-			expect(test.mixin.Test.prototype.saySomethingElse()).toEqual("mixed in method");
-		});
-		
-		it("should overwrite mixin methods with own methods", function() {
-		  expect(test.mixin.Test.prototype.sayHello()).toEqual("hello world");
-		});
-	});
-	
+	}); // end Class
 });

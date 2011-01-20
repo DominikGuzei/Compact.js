4900ba428ddb2c1ea2c98c286ff041f810d43f4e
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-20 01:33:44 +0100

    Add find function taken from underscore.js

---
+++ scripts/utility/collection/find.js
@@ -0,0 +1,30 @@
+/**
+ * This gracefully taken from the Underscore.js library
+ * http://documentcloud.github.com/underscore/
+ */
+
+define(['utility/collection/some'], function(some) {
+
+	/** 
+	* 
+	 * find
+	 * 
+	 * Looks through each value in the list, returning the first one that passes a truth 
+	 * test (iterator). The function returns as soon as it finds an acceptable element, 
+	 * and doesn't traverse the entire list.
+	 * 
+	 * @param {Object/Array} obj The object or array that is searched
+	 * @param {Function} iterator The function that gets called for each property/index
+	 * @param {Object} context The context the iterator is bound to
+	 */
+	return function(obj, iterator, context) {
+    var result;
+    some(obj, function(value, index, list) {
+      if (iterator.call(context, value, index, list)) {
+        result = value;
+        return true;
+      }
+    });
+    return result;
+  };
+});
---
+++ tests/spec/scripts/utility/collection/findSpec.js
@@ -0,0 +1,18 @@
+define(['utility/collection/find'], function(find) {
+	
+	describe("utility/collection/find", function() {
+		
+		it("Returns the first value where the iterator returns true", function() {
+		  var array = [1,2,3];
+			expect( find(array, function(value){return value == 2;}) ).toEqual(2);
+			var test = {
+				first: 1,
+				second: 2,
+				third: 3
+			};
+			expect( find(test, function(value){return value == 2;}) ).toEqual(2);
+		});
+	
+	});
+	
+});

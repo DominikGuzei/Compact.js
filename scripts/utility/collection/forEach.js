01e7d0f5fffcf7ae5156796632406b43ed30a5eb
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-20 01:31:46 +0100

    Removed breaker object and use simple false value for breaking the forEach loop

--- scripts/utility/collection/forEach.js
+++ scripts/utility/collection/forEach.js
@@ -1,25 +1,42 @@
-define(function() {
+/**
+ * This gracefully taken from the Underscore.js library
+ * http://documentcloud.github.com/underscore/
+ */
+
+define(['utility/typeof'], function(type) {
 	
-	var nativeforEach = Array.prototype.forEach,
-			breaker = {};
+	var nativeforEach = Array.prototype.forEach;
 
 	/** 
+	 * forEach
+	 * 
+	 * Iterates over a list of elements, yielding each in turn to an 
+	 * iterator function. The iterator is bound to the context object, 
+	 * if one is passed. Each invocation of iterator is called with 
+	 * three arguments: (element, index, list). If list is a JavaScript 
+	 * object, iterator's arguments will be (value, key, list). 
+	 * Delegates to the native forEach function if it exists.
+	 * 
 	 * Handles objects implementing `forEach`, arrays, and raw objects.
 	 * Delegates to **ECMAScript 5**'s native `forEach` if available.
+	 * 
+	 * @param {Object/Array} obj The object or array to iterate over
+	 * @param {Function} iterator The function that gets called for each property/index
+	 * @param {Object} context The context the iterator is bound to
 	 */
 	return function(obj, iterator, context) {
 	  var value;
     if (obj == null) return;
     if (nativeforEach && obj.forEach === nativeforEach) {
       obj.forEach(iterator, context);
-    } else if (_.isNumber(obj.length)) {
+    } else if (type.isNumber(obj.length)) {
       for (var i = 0, l = obj.length; i < l; i++) {
-        if (iterator.call(context, obj[i], i, obj) === breaker) return;
+        if (iterator.call(context, obj[i], i, obj) == false) return;
       }
     } else {
       for (var key in obj) {
         if (hasOwnProperty.call(obj, key)) {
-          if (iterator.call(context, obj[key], key, obj) === breaker) return;
+          if (iterator.call(context, obj[key], key, obj) == false) return;
         }
       }
     }
--- tests/spec/scripts/utility/collection/forEachSpec.js
+++ tests/spec/scripts/utility/collection/forEachSpec.js
@@ -9,6 +9,18 @@ define(['utility/collection/forEach'], function(forEach) {
 			});
 			expect(array).toEqual([2,3,4]);
 		});
+		
+		it("Handles every element of an object", function() {
+		  var test = {
+				first: 1,
+				second: 2,
+				third: 3
+			};
+			forEach(test, function(property, key, object) {
+				object[key] = ++property;
+			});
+			expect(test).toEqual({first:2,second:3,third:4});
+		});
 	
 	});
 	

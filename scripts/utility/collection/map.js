46fb6763c0840fb89ca08ccacfc8ead32ff9f73c
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-20 01:32:48 +0100

    Add map function taken from underscore.js

---
+++ scripts/utility/collection/map.js
@@ -0,0 +1,35 @@
+/**
+ * This gracefully taken from the Underscore.js library
+ * http://documentcloud.github.com/underscore/
+ */
+
+define(['utility/collection/forEach'], function(forEach) {
+	
+	var nativeMap = Array.prototype.map;
+
+	/** 
+	 * map
+	 * 
+	 * Produces a new array of values by mapping each value in list through 
+	 * a transformation function (iterator). If the native map method exists, 
+	 * it will be used instead. If list is a JavaScript object, iterator's 
+	 * arguments will be (value, key, list).
+	 * 
+	 * Return the results of applying the iterator to each element.
+	 * Delegates to **ECMAScript 5**'s native `map` if available.
+	 * 
+	 * @param {Object/Array} obj The object or array that is mapped into the result array
+	 * @param {Function} iterator The function that gets called for each property/index
+	 * @param {Object} context The context the iterator is bound to
+	 */
+	return function(obj, iterator, context) {
+	  var results = [];
+    if (obj == null) return results;
+    if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
+    forEach(obj, function(value, index, list) {
+      results[results.length] = iterator.call(context, value, index, list);
+    });
+    return results;
+  };
+	
+});
---
+++ tests/spec/scripts/utility/collection/mapSpec.js
@@ -0,0 +1,27 @@
+define(['utility/collection/map'], function(map) {
+	
+	describe("utility/collection/map", function() {
+		
+		it("Returns a new array with each value iterated over", function() {
+		  var array = [1,2,3];
+			expect(map(array, function(number, index, array) {
+					return number + 1;
+				})
+			).toEqual([2,3,4]);
+		});
+		
+		it("Returns a new array with each property iterated over", function() {
+		  var test = {
+				first: 1,
+				second: 2,
+				third: 3
+			};
+			expect(map(test, function(property, key, object) {
+					return property + 1;
+				})
+			).toEqual([2,3,4]);
+		});
+	
+	});
+	
+});

7956e4183bdcd721ececda1676c467e59a4365a6
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-20 02:01:45 +0100

    Added 'synchronize' to Store as hook for persistence classes to handle model changes

--- scripts/model/Store.js
+++ scripts/model/Store.js
@@ -2,19 +2,36 @@ define(['Class', 'events/EventDispatcher'], function(Class, EventDispatcher) {
 	
 	var namespace = this;
 	
-	Class("Storage") .mixin(EventDispatcher)
-	
+	Class("Store") .mixin(EventDispatcher)
+	.methods({
+		synchronize: function(method, model) {
+			switch(method) {
+				case "create":
+					this.onEvent("create", model);
+					break;
+				case "read":
+					this.onEvent("read", model);
+					break;
+				case "update":
+					this.onEvent("update", model);
+					break;
+				case "delete":
+					this.onEvent("delete", model);
+					break;
+			}
+		}
+	})
 	.statics({
 		instance: undefined,
-		get: function() {
-			if(!namespace.Storage.instance) {
-				namespace.Storage.instance = new namespace.Storage();
+		getInstance: function() {
+			if(!namespace.Store.instance) {
+				namespace.Store.instance = new namespace.Store();
 			}
-			return namespace.Storage.instance;
+			return namespace.Store.instance;
 		}
 	})
 	.end(namespace);
 	
-	return namespace.Storage;
+	return namespace.Store;
 	
 });
--- tests/spec/scripts/model/StoreSpec.js
+++ tests/spec/scripts/model/StoreSpec.js
@@ -1,16 +1,62 @@
-define(['model/Storage'], function(Storage) {
+define(['model/Store', 'model/Model'], function(Store, Model) {
 	
-	describe("model/Storage", function() {
-	  var storage;
+	describe("model/Store", function() {
+	  var store;
 		beforeEach(function() {
-		  storage = Storage.get();
+		  store = Store.getInstance();
 		});
 		
-		describe("get", function() {
+		describe("getInstance", function() {
 		  
 			it("Returns the single instance of Storage singleton", function() {
-				expect(storage).toBe(Storage.instance);
-				expect(storage instanceof Storage).toBeTruthy();
+				expect(store).toBe(Store.instance);
+				expect(store instanceof Store).toBeTruthy();
+			});
+		
+		});
+	
+		describe("synchronize", function() {
+		  
+			var model = new Model();
+			var called;
+			beforeEach(function() {
+			  called = false;
+			});
+		
+			it("Dispatches an event that a model/collection should be created", function() {
+				store.on("create", function(modelToCreate) {
+					expect(modelToCreate).toBe(model);
+					called = true;
+				});
+				store.synchronize("create", model);
+				expect(called).toBeTruthy();
+			});
+			
+			it("Dispatches an event that a model/collection should be fetched from a server/storage", function() {
+				store.on("read", function(modelToCreate) {
+					expect(modelToCreate).toBe(model);
+					called = true;
+				});
+				store.synchronize("read",model);
+				expect(called).toBeTruthy();
+			});
+			
+			it("Dispatches an event that a model/collection should be updated to a server/storage", function() {
+				store.on("update", function(modelToCreate) {
+					expect(modelToCreate).toBe(model);
+					called = true;
+				});
+				store.synchronize("update",model);
+				expect(called).toBeTruthy();
+			});
+			
+			it("Dispatches an event that a model/collection should be deleted on the server/storage", function() {
+				store.on("delete", function(modelToCreate) {
+					expect(modelToCreate).toBe(model);
+					called = true;
+				});
+				store.synchronize("delete",model);
+				expect(called).toBeTruthy();
 			});
 		
 		});

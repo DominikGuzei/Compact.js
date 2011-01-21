e72de3bbaa474086813bfe0cf9eba1329437f3f9
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-21 15:51:58 +0100

    Add getValues function that returns an array of object property values

---
+++ scripts/utility/object/getValues.js
@@ -0,0 +1,20 @@
+/**
+ * This gracefully taken from the Underscore.js library
+ * http://documentcloud.github.com/underscore/
+ */
+
+define(['utility/collection/map'], function(map) {
+
+	/**
+	 * getValues(
+	 * )
+	 * Retrieve the values of an object's properties.
+	 * 
+	 * @param {Object} subject The subject to return the values from
+	 */ 
+	return function(subject) {
+	  return map(subject, function(value) {
+			return value;
+		});
+	};
+});
---
+++ tests/spec/scripts/utility/object/getValuesSpec.js
@@ -0,0 +1,16 @@
+define(['utility/object/getValues'], function(getValues) {
+	
+	describe("utility/object/getValues", function() {
+		
+		it("Returns true if the iterator returs true for any value", function() {
+			var test = {
+				first: 1,
+				second: 2,
+				third: 3
+			};
+			expect( getValues(test) ).toEqual([1,2,3]);
+		});
+	
+	});
+	
+});
